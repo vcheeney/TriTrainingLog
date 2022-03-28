ae1fdc07f2cf9ec4f63f39b33e7336cd217d41d0
Victor Cheeney <victorcheeney@gmail.com>
2022-03-28 16:11:29 -0400

    Ran prettier on all frontend files (npm run format)

--- frontend/src/routes/__layout.svelte
+++ frontend/src/routes/__layout.svelte
@@ -1,11 +1,15 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
 
 <div class="bg-white font-semibold shadow-xl">
 	<div class="container max-w-screen-sm mx-auto p-4 flex justify-between items-center">
-    <h1 class="text-black text-xl font-bold">TRI <span class="text-red-500">TRAINING</span> LOG</h1>
-    <a href="https://github.com/vcheeney/TriTrainingLog" target="blank" class="text-gray-500 text-sm underline">GitHub</a>
+		<h1 class="text-black text-xl font-bold">TRI <span class="text-red-500">TRAINING</span> LOG</h1>
+		<a
+			href="https://github.com/vcheeney/TriTrainingLog"
+			target="blank"
+			class="text-gray-500 text-sm underline">GitHub</a
+		>
 	</div>
 </div>
 
